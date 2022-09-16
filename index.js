730e13286a401fcc589e165a287d44c34434830f
iSaacSigei <kiplangatisack.is@gmail.com>
2022-09-16 20:11:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  constructor(option) {
+    this.option = option;
+  }
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9- ']/g, "");
+  }
+
+  static titleize(string) {
+    const words = string.split(" ");
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const title = words.map((word) => {
+      if (exceptions.includes(word)) {
+        return word;
+      } else {
+        return this.capitalize(word);
+      }
+    });
+    return this.capitalize(title.join(" "));
+  }
 }
